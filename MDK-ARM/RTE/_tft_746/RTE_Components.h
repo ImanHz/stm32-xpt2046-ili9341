30b70ef20abb227518592560b94a1ea5cf91f9f6
Iman H <iman.hosseinzadeh@gmail.com>
2021-05-20 11:32:05 +0430

    README.md added

--- MDK-ARM/RTE/_tft_746/RTE_Components.h
+++ MDK-ARM/RTE/_tft_746/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Component Configuration File
+ * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'tft_746' 
@@ -10,11 +10,6 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32f7xx.h"
 
 
 #endif /* RTE_COMPONENTS_H */
---
+++ README.md
@@ -0,0 +1,7 @@
+# Sample program for STM32F7 Nucleo + ILI9341 LCD + XPT2046 SPI Touch IC
+
+This program could be ported to other STM32 mcus easily.
+
+XPT2046 is a cheap clone for ADS7846 touch IC.
+
+STM32CubeMX file is also included and could be used to adopt the connections.
